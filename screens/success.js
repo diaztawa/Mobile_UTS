4c4aeb55a38eebf035af5b084d6da2472c5d5db0
diaztawa <diaztawa3@gmail.com>
2021-01-20 13:33:34 +0800

    Revert "Navigation update"
    This reverts commit ba0541623ab6c1642ae87ada71a998a253ba9aff.

--- screens/success.js
+++ screens/success.js
@@ -4,15 +4,14 @@ import MainScreen from './Main/Main';
 import { CardStyleInterpolators } from '@react-navigation/stack';
 
 export default [
-  
-  {
-    name: 'Main',
-    component: MainScreen,
-    options: { headerShown: false },
-  },
   {
     name: 'Home',
     component: HomeScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Main',
+    component: MainScreen,
     options: { headerShown: false },
   },
   {
